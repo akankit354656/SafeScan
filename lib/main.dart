14ca7e912080c129769eff0293f29a759ea86901
akankit354656 <aksheoran1024@gmail.com>
2026-02-18 11:56:59 +0530

    some ui changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,175 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:qr_scanner_app/scanscreen/scanscreen.dart';
-// import 'theme/theme_provider.dart';
-// import 'package:qr_scanner_app/splash/splash.dart';
-// import 'package:qr_scanner_app/theme/theme_provider.dart';
-// import 'settings/settings.dart';
 
-// import 'package:flutter/material.dart';
-// import 'package:qr_scanner_app/scanscreen/scanscreen.dart';
-// import 'theme/theme_provider.dart';
-
-// void main() {
-//   runApp(
-//       const MyApp(),
-//     );
-
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Safe Scan',
-//       debugShowCheckedModeBanner: false,
-
-//       home: SplashScreen(),
-//     );
-//   }
-// }
-
-// class HomePage extends StatefulWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       // backgroundColor:  const Color.fromARGB(255, 0, 253, 114),
-//       appBar: AppBar(
-//         title: const Text("Safe Scan",style:TextStyle(color: Color.fromARGB(255, 227, 227, 227),fontWeight: FontWeight.bold,),),
-//         backgroundColor: const Color.fromARGB(255, 69, 104, 130),
-
-//         actions: [
-//           IconButton(icon: const Icon(Icons.image,color: Color.fromARGB(255, 227, 227, 227),), onPressed: () {}),
-//           IconButton(icon: const Icon(Icons.flashlight_on,color: Color.fromARGB(255, 227, 227, 227),), onPressed: () {}),
-//           IconButton(icon: const Icon(Icons.camera_alt,color: Color.fromARGB(255, 227, 227, 227),), onPressed: () {}),
-//         ],
-//       ),
-//       drawer: Drawer(
-//         child: Container(
-//           decoration: const BoxDecoration(
-//             gradient: LinearGradient(
-//               colors: [
-//                Color.fromARGB(255, 27, 60, 83),
-//                 Color.fromARGB(255, 0, 46, 79)
-
-//               ],
-//               begin: Alignment.bottomCenter,
-//               end: Alignment.topCenter,
-//             ),
-//           ),
-
-// child: ListView(
-//   padding: const EdgeInsets.only(top: 50),
-//   children: [
-//     ListTile(
-//       leading: const Icon(
-//         Icons.star_border_rounded,
-//         color: Color.fromARGB(255, 255, 255, 255),
-//         size: 28,
-//       ),
-//       title: const Text(
-//         'Favourites',
-//         style: TextStyle(
-//           color: Color.fromARGB(255, 255, 255, 255),
-//           fontWeight: FontWeight.bold,
-//         ),
-//       ),
-//       onTap: () => Navigator.pop(context), // Closes the drawer
-//     ),
-//     ListTile(
-//       leading: const Icon(
-//         Icons.settings_rounded,
-//         color: Color.fromARGB(255, 253, 253, 253),
-//         size: 28,
-//       ),
-//       title: const Text(
-//         'Settings',
-//         style: TextStyle(
-//           color: Color.fromARGB(255, 255, 255, 255),
-//           fontWeight: FontWeight.bold,
-//         ),
-//       ),
-//       onTap: () {},
-//     ),
-//     ListTile(
-//       leading: const Icon(
-//         Icons.history_rounded,
-//         color: Color.fromARGB(255, 255, 255, 255),
-//         size: 28,
-//       ),
-//       title: const Text(
-//         'History',
-//         style: TextStyle(
-//           color: Color.fromARGB(255, 255, 255, 255),
-//           fontWeight: FontWeight.bold,
-//         ),
-//       ),
-//       onTap: () {},
-//     ),
-//   ],
-// ),
-//         ),
-//       ),
-//       body: Container(
-//         decoration: const BoxDecoration(
-//           gradient: LinearGradient(
-//             colors: [
-//               Color.fromARGB(255, 27, 60, 83),
-//                 Color.fromARGB(255, 35, 76, 106)
-//             ],
-//             begin: Alignment.bottomCenter,
-//             end: Alignment.topCenter,
-//           ),
-//         ),
-//         child: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               const Text(
-//                 "Welcome to Safe Scan",
-//                 style: TextStyle(
-//                   fontSize: 24,
-//                   fontWeight: FontWeight.bold,
-//                   color: Color.fromARGB(255, 255, 255, 255),
-//                 ),
-//               ),
-//               const SizedBox(height: 30),
-
-//               // 👇 BUTTON
-//               ElevatedButton(
-//                 onPressed: () {
-//                   Navigator.push(
-//                     context,
-//                     MaterialPageRoute(builder: (context) => const ScanScreen()),
-//                   );
-//                 },
-//                 style: ElevatedButton.styleFrom(
-//                   padding: const EdgeInsets.symmetric(
-//                     horizontal: 40,
-//                     vertical: 15,
-//                   ),
-//                   backgroundColor: const Color.fromARGB(255, 227, 227, 227)
-
-//                 ),
-//                 child: const Text(
-//                   "Start Scanning",
-//                   style: TextStyle(fontSize: 18, color: Colors.black,fontWeight: FontWeight.bold,),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 import 'package:flutter/material.dart';
 import 'package:qr_scanner_app/screens/history_screen.dart';
@@ -217,7 +46,7 @@ class _HomePageState extends State<HomePage> {
             fontWeight: FontWeight.bold,
           ),
         ),
-        backgroundColor: const Color.fromARGB(255, 69, 104, 130),
+        backgroundColor: const Color.fromARGB(255, 43, 54, 63),
         actions: [
           IconButton(
             icon: const Icon(
@@ -249,8 +78,8 @@ class _HomePageState extends State<HomePage> {
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Color.fromARGB(255, 27, 60, 83),
-                Color.fromARGB(255, 0, 46, 79),
+                Color.fromARGB(255, 17, 66, 101),
+                Color.fromARGB(255, 0, 5, 9),
               ],
               begin: Alignment.bottomCenter,
               end: Alignment.topCenter,
@@ -324,8 +153,8 @@ class _HomePageState extends State<HomePage> {
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              Color.fromARGB(255, 27, 60, 83),
-              Color.fromARGB(255, 35, 76, 106),
+              Color.fromARGB(255, 17, 67, 103),
+              Color.fromARGB(255, 0, 22, 38),
             ],
             begin: Alignment.bottomCenter,
             end: Alignment.topCenter,
